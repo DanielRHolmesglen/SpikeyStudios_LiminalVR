1350eaca552a3266178e2904b85fc01346fd3bd1
JsGettyhub <yoshiman2107@gmail.com>
2023-08-10 12:05:14 +1000

    first test.

---
+++ Assets/Scripts/BiggerBox.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class BiggerBox : MonoBehaviour, IPointerClickHandler,
+    IPointerEnterHandler, IPointerExitHandler, IPointerUpHandler, IPointerDownHandler
+{
+    public UnityEvent OnInteraction;
+
+    void Start()
+    {
+
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        OnInteraction.Invoke();
+        transform.Rotate(0,30,0);
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        transform.localScale *= 1.2f;
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        transform.localScale *= 1;
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        transform.localScale *= 1;
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        transform.localScale *= 0.5f;
+    }
+}
