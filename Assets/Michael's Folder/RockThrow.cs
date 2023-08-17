5d8ea03b09d6dc64d47d0f346e96ecfefbbadaf9
Shiroinu25 <gunnzlinger@gmail.com>
2023-08-17 11:50:43 +1000

    Update 0.1
    Added some basic scripts
    
    (committed 17/08)

---
+++ Assets/Michael's Folder/RockThrow.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockThrow : MonoBehaviour
+{
+    public GameObject rock;
+    
+    float launchSpeed
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       RockThrow = 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //throw when button is released
+        Input.GetButtonUp(One){
+            Throw();
+        }
+    }
+
+    public void Throw(GameObject rock)
+    {
+        //instantiate object with launchSpeed and direction of player
+        Instantiate(rock); 
+    } 
+
+    public void 
+}
---
+++ Assets/Michael's Folder/Rockmechanics/RockSkip.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEditor;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class RockSkip : MonoBehaviour
+{
+    /// <summary>
+    /// rock skip mechanic
+    /// rock continues to skip when above horizontal speed threshold 
+    /// </summary>
+
+    //objects
+    public GameObject rock;
+    public GameObject player;
+
+    //speed variables
+    public float hMinSpeed; //minimum speed threshold
+    public float hSpeed; //current horizontal speed
+    public float airDrag; //speed reduction overtime
+
+    Vector3 rockForward; //rock forward direction
+
+    void Start()
+    {
+        rock = GetComponent<GameObject>();
+
+    }
+
+    void Update()
+    {
+        // if speed is greater than threshold, then skip over water
+        if (hSpeed > hMinSpeed)
+        {
+            //move forward relative to the object's forward
+            transform.position = hSpeed * player.transform.forward * Time.deltaTime;
+
+            if () { }
+                RockSkip();
+            hSpeed -= airDrag * Time.deltaTime;
+        }
+        else
+        {
+            RockSink();
+        }
+    }
+
+    public void RockSink()
+    {
+        //sink rock below water level, apply vfx, and destroy object
+        Invoke("Destroy(this.gameObject)",2f);
+    }
+
+    public void RockSkip()
+    {
+        //apply vfx, skip over water
+    }
+}
