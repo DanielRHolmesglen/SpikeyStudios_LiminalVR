ba2da9bd8c773e3464a031a3c8040b263f6329cb
DeliciousPancakes1234 <Tinybearbyworld@gmail.com>
2023-10-10 13:00:43 +1100

    Moving stuff around

---
+++ Assets/Testing stuff(Natalies files)/Scipts/RippleEffect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RippleEffect : MonoBehaviour
+{
+    public ParticleSystem splashEffect;
+    public GameObject water;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == water)
+        {
+            splashEffect.Stop();
+            splashEffect.transform.position = collision.contacts[0].point;
+            splashEffect.transform.position += Vector3.up * 0.1f;
+            splashEffect.Play();
+        }
+    }
+}
