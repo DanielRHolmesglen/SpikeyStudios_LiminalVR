0b5968a6fc7770c4277bcdb6969af4037209c792
DeliciousPancakes1234 <Tinybearbyworld@gmail.com>
2023-10-24 11:44:32 +1100

    added a script

---
+++ Assets/Testing stuff(Natalies files)/Scipts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Liminal.Core.Fader;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] rocksToSpawn;
+    public float numberOfRocksToSpawn;
+    public bool isRandomized = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isRandomized == true)
+        {
+            int randomIndex = Random.Range(0, rocksToSpawn.Length);
+            
+            Instantiate(rocksToSpawn[randomIndex], transform.position, Quaternion.identity);
+        }
+    }
+
+    void SpawnRocks()
+    {
+        
+    }
+}
